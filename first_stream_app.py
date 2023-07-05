0de2eb70f2653cd19ba834ee8c10c879a7831978
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-07-05 15:38:06 +0530

    Update first_stream_app.py

--- first_stream_app.py
+++ first_stream_app.py
@@ -1,6 +1,4 @@
 import streamlit
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
 
