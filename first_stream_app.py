74f5ce3b7239ea030db3d1ffde6bb13228f84906
sravanisammu23 <70464115+sravanisammu23@users.noreply.github.com>
2023-07-05 15:50:07 +0530

    Update first_stream_app.py

--- first_stream_app.py
+++ first_stream_app.py
@@ -12,4 +12,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index('Fruit')
-my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
